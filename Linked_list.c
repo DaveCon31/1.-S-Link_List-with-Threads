eb0d962d6acb303356e7e75d6dab8b3068a1decf
DaveCon31 <davidviorel54@yahoo.com>
2021-11-22 13:45:09 +0200

    ver 0.3

--- Linked_list.c
+++ Linked_list.c
@@ -2,202 +2,128 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <semaphore.h>
+#include "ll_API.h"
+#include <time.h>
 
+enum { NUM_THREADS = 3
+};
 
-typedef struct node{
-	int val;
-	void (*callback)(int);	//argument type can be used as void* for generic type
-	struct node *next;
-} node_t;			//node_t as node data structure type	
-	
-node_t *head = NULL;		//global to have 1 argument for: add and del @alternative: use as static
-node_t *last_node = NULL;	//global to have 1 argument for: add and del @alternative: use as static
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+pthread_barrier_t barrier;					//threads sync for creation using barrier
+int current = 0;
 
-
-
-
-void print_int(int element)
-{
-	printf("%d ", element);
-}
-
-
-
-
-void add(int value)
-{
-
-	node_t *new_node = malloc(sizeof(node_t));		//use calloc? or Hirschmann malloc?
-	if (NULL == new_node){
-		printf("Memory allocation for new_node failed! (add) \n");
-	}
-	
-	new_node->next = NULL;
-	new_node->val = value;
-	new_node->callback = print_int;		//set function pointer
-	if (NULL != last_node){				//link new_node with last node if linked list is not empty
-		last_node->next = new_node;
-	}
-	
-	if (NULL == head){			
-		head = new_node;			//set first node as head if linked list is empty
-	}
-	
-	last_node = new_node;
-	printf("Node with value: %d added!\n",value);
-	
-}
-
-
-
-
-void delete(int value)
-{
-
-	node_t *temp = head;
-	int count_element = 0, flag_delete = 0;
-	node_t *previous_node = malloc(sizeof(node_t));
-	
-	if (NULL == previous_node){
-		printf("Memory allocation for previous_node failed! (delete)\n");
-	}
-	
-	while (NULL != temp)
-	{
-		count_element++;
-		if (temp->val == value)
-		{
-			printf("Deleting Node no. %d with value: %d\n",count_element,value);
-			previous_node->next = temp->next;
-			free(temp);
-			
-			flag_delete = 1;
-		}
-		
-		if (1 == flag_delete)	break;		//deleting only 1 node (first found) if multiple nodes have the same value
-		previous_node = temp;
-		temp = temp->next;
-	}
-	
-	if (0 == flag_delete){
-		printf("Searched node doesn't exist!\n");
-	}
-
-}
-
-
-
-
-void swap(node_t *a, node_t *b)			//used for sort_list
-{
-	int temp = a->val;
-	a->val = b->val;
-	b->val = temp;
-
-}
-
-void sort_list(void)					//bubble sort
-{
-	int swapped, i;
-	node_t *ptr2 = NULL;
-	node_t *ptr1 = malloc(sizeof(node_t));
-	if (NULL == ptr1){
-		printf("Memory allocation for previous_node failed! (sort_list)\n");
-	}
-	
-	if (NULL == head)	return;		//exit if linked list is empty
-	
-	do
-	{
-		swapped = 0;
-		ptr1 = head;
-		
-		while (ptr1->next != ptr2)
-		{
-			if (ptr1->val > ptr1->next->val)
-			{
-				swap(ptr1, ptr1->next);
-				swapped = 1;
-			}
-			ptr1 = ptr1->next;
-		}
-		ptr2 = ptr1;
-	}
-	while (swapped);
-	
-	printf("Linked list has been sorted! \n");
-}
-
-
-
-void flush_list(void)
-{
-	node_t *temp, *cursor = malloc(sizeof(node_t));
-	if(temp || cursor == NULL){
-		printf("Memory allocation failed! (flush_list)\n");
-	}
-	 
-	if (NULL != head)
-	{
-		cursor = head->next;
-		head->next = NULL;
-		while (NULL != cursor)
-		{
-			temp = cursor->next;
-			free(cursor);
-			cursor = temp;
-		
-		} 
-	}
-	
-	head = NULL;					//flush first element too
-	printf("Linked list has been deleted!\n");
-	
-}
-
-
-
-void print_list(void)
+void *sync_routine(void *arg)				
 {	
-	
-	node_t *temp = head;
 
-	printf("Printing linked list ... \n");
 	
-	if (NULL == head){				//check if list is empty to prevent seg fault
-		printf("List is empty !!! Add new elements.\n");
-		return;
-	}
+	int internal_tid = *(int*) arg;
+	printf("Waiting at the barrier... Thread no: %d\n", internal_tid+1);
+	pthread_barrier_wait(&barrier);			//wait to create all threads before executing 
 	
+	pthread_mutex_lock(&lock);
+	printf("We passed the barrier! Thread no: %d\n", internal_tid+1);
+	pthread_mutex_unlock(&lock);
 	
-	while (NULL != temp)
+	pthread_mutex_lock(&lock);
+	while(internal_tid > current)	pthread_cond_wait(&cond, &lock);	
+	
+	switch(internal_tid)
 	{
-		temp->callback(temp->val);		//use function pointer	
-		temp = temp->next;
-		if (temp!=NULL)	printf(" ---> ");
+		case 0:
+			delete(2);
+			sort_list();
+			flush_list();
+			print_list();
+			printf("\nThread: %d ===================== \n",internal_tid+1);
+			add(2);
+			print_list();
+			add(4);
+			print_list();
+			add(10);
+			print_list();
+			delete(2);
+			print_list();
+			sort_list();
+			print_list();
+			delete(10);
+			print_list();
+			delete(5);
+			print_list();
+			break;
+			
+		case 1:
+			printf("\nThread: %d ===================== \n",internal_tid+1);
+			add(11);
+			print_list();
+			add(1);
+			print_list();
+			delete(11);
+			print_list();
+			add(8);
+			print_list();
+			break;
+		
+		case 2:
+			printf("\nThread: %d ===================== \n",internal_tid+1);
+			add(30);
+			print_list();
+			add(25);
+			print_list();
+			add(100);
+			print_list();
+			sort_list();
+			print_list();
+			delete(100);
+			print_list();
+			break;
+	
 	}
-	
-	printf("\n");
-	
+	current++;
+	pthread_cond_broadcast(&cond);
+	pthread_mutex_unlock(&lock);
+	free(arg);
+	printf("Thread exited?!\n");
+	return (void*)0;
 }
-
-
 	
+	
+
 int main()
 {
-
-	add(7);
-	add(10);
-	add(11);
-	add(1);
-	delete(10);
+	pthread_t th[NUM_THREADS-1];
+	int i;
+	pthread_barrier_init(&barrier, NULL, NUM_THREADS);
+	pthread_mutex_init(&lock, NULL);
+	pthread_cond_init(&cond, NULL);
 	
-	print_list();
-	sort_list();
+	callback_validate = validate;		//set function pointer to validate head
+		
+	for (i = 0; i < NUM_THREADS; i++) {
+		int *a = malloc(sizeof(int)); 
+		*a = i;
+		if (pthread_create(&th[i], NULL, &sync_routine, a) != 0) {
+			perror("Failed to create thread!\n");
+		}
+	}
+	
+	for (i = 0; i < NUM_THREADS; i++) {
+		if (pthread_join(th[i], NULL) != 0) {
+			perror("Failed to join thread!\n");
+		}
+	}
+	
+	pthread_barrier_destroy(&barrier);
+	pthread_mutex_destroy(&lock);
+	pthread_cond_destroy(&cond);
+	
+	printf("\n----------------------------\nThreads executions finished!\n");
 	print_list();
 	flush_list();
 	print_list();
-	
+	return 0;
 }
 
 
