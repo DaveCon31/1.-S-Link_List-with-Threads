5654b7d0562d75bad49e50bfb5ab8a388d753a8e
DaveCon31 <davidviorel54@yahoo.com>
2021-11-23 15:57:18 +0200

    Updated

--- Linked_list.c
+++ Linked_list.c
@@ -1,24 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <semaphore.h>
 #include "ll_API.h"
-#include <time.h>
 
-enum { NUM_THREADS = 3
-};
+#define NUM_THREADS 3
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 pthread_barrier_t barrier;					//threads sync for creation using barrier
 int current = 0;
+//int (*callback_validate)(void *name);    //VALIDATE HEAD
 
 void *sync_routine(void *arg)				
 {	
-
-	
+	ll_t l1, l2, l3;
 	int internal_tid = *(int*) arg;
 	printf("Waiting at the barrier... Thread no: %d\n", internal_tid+1);
 	pthread_barrier_wait(&barrier);			//wait to create all threads before executing 
@@ -28,59 +23,69 @@ void *sync_routine(void *arg)
 	pthread_mutex_unlock(&lock);
 	
 	pthread_mutex_lock(&lock);
-	while(internal_tid > current)	pthread_cond_wait(&cond, &lock);	
+	while (internal_tid > current) {
+		pthread_cond_wait(&cond, &lock);	
+	}
 	
-	switch(internal_tid)
-	{
+	switch(internal_tid) {
 		case 0:
-			delete(2);
-			sort_list();
-			flush_list();
-			print_list();
+			ll_create(&l1);
+			ll_delete(&l1, 2);
+			ll_sort_list(&l1);
+			ll_flush_list(&l1);
+			ll_print_list(&l1);
 			printf("\nThread: %d ===================== \n",internal_tid+1);
-			add(2);
-			print_list();
-			add(4);
-			print_list();
-			add(10);
-			print_list();
-			delete(2);
-			print_list();
-			sort_list();
-			print_list();
-			delete(10);
-			print_list();
-			delete(5);
-			print_list();
+			ll_add_end(&l1, 2);
+			ll_print_list(&l1);
+			ll_add_end(&l1, 4);
+			ll_print_list(&l1);
+			ll_add_end(&l1, 10);
+			ll_print_list(&l1);
+			ll_delete(&l1, 2);
+			ll_print_list(&l1);
+			ll_sort_list(&l1);
+			ll_print_list(&l1);
+			ll_delete(&l1, 10);
+			ll_print_list(&l1);
+			ll_delete(&l1, 5);
+			ll_print_list(&l1);
+			ll_flush_list(&l1);
+			ll_print_list(&l1);
 			break;
 			
 		case 1:
+			ll_create(&l2);
 			printf("\nThread: %d ===================== \n",internal_tid+1);
-			add(11);
-			print_list();
-			add(1);
-			print_list();
-			delete(11);
-			print_list();
-			add(8);
-			print_list();
+			ll_add_end(&l2, 11);
+			ll_print_list(&l2);
+			ll_add_end(&l2, 1);
+			ll_print_list(&l2);
+			ll_delete(&l2, 11);
+			ll_print_list(&l2);
+			ll_add_end(&l2, 8);
+			ll_print_list(&l2);
+			ll_flush_list(&l2);
+			ll_print_list(&l2);
 			break;
 		
 		case 2:
+			ll_create(&l3);
 			printf("\nThread: %d ===================== \n",internal_tid+1);
-			add(30);
-			print_list();
-			add(25);
-			print_list();
-			add(100);
-			print_list();
-			sort_list();
-			print_list();
-			delete(100);
-			print_list();
+			ll_add_end(&l3, 30);
+			ll_print_list(&l3);
+			ll_add_end(&l3, 25);
+			ll_print_list(&l3);
+			ll_add_end(&l3, 100);
+			ll_print_list(&l3);
+			ll_sort_list(&l3);
+			ll_print_list(&l3);
+			ll_delete(&l3, 100);
+			ll_print_list(&l3);
+			ll_flush_list(&l3);
+			ll_print_list(&l3);
 			break;
-	
 	}
+	
 	current++;
 	pthread_cond_broadcast(&cond);
 	pthread_mutex_unlock(&lock);
@@ -90,7 +95,6 @@ void *sync_routine(void *arg)
 }
 	
 	
-
 int main()
 {
 	pthread_t th[NUM_THREADS-1];
@@ -98,9 +102,8 @@ int main()
 	pthread_barrier_init(&barrier, NULL, NUM_THREADS);
 	pthread_mutex_init(&lock, NULL);
 	pthread_cond_init(&cond, NULL);
+	callback_validate = validate;
 	
-	callback_validate = validate;		//set function pointer to validate head
-		
 	for (i = 0; i < NUM_THREADS; i++) {
 		int *a = malloc(sizeof(int)); 
 		*a = i;
@@ -120,9 +123,9 @@ int main()
 	pthread_cond_destroy(&cond);
 	
 	printf("\n----------------------------\nThreads executions finished!\n");
-	print_list();
-	flush_list();
-	print_list();
+	/*ll_print_list();
+	ll_flush_list();
+	ll_print_list();*/
 	return 0;
 }
 
