2f25c17fbfce44de9b010149cbdab7080ca7953c
DaveCon31 <davidviorel54@yahoo.com>
2021-11-25 15:53:40 +0200

    Implemented ll_search_node function + implemented DEBUG_PRINTF which enables/disables printfs

--- ll_source.c
+++ ll_source.c
@@ -3,12 +3,20 @@
 #include <string.h>
 #include "ll_API.h"
 
+#define NDEBUG    //disable/enable printfs (comment to enable printfs)
+
+#ifdef NDEBUG
+#define DEBUG_PRINTF(...)
+#else
+#define DEBUG_PRINTF(...) printf(__VA_ARGS__)
+#endif
+
 int (*callback_validate)(void *data) = NULL;    //Validate data type; set by user
 
 int validate_head(ll_t *list, void *name)    //VALIDATE HEAD list
 {   
 	if (list->head == NULL) {
-		printf("Linked list is empty! (%s)\n", (const char*)name);
+		DEBUG_PRINTF("Linked list is empty! (%s)\n", (const char*)name);
 		return -1;
 	}
 	return 0;
@@ -16,11 +24,11 @@ int validate_head(ll_t *list, void *name)    //VALIDATE HEAD list
 
 void ll_create(ll_t* list, void (*ptr_print_val)(void *val)) 
 {
-	printf("Creating linked list ...");
+	DEBUG_PRINTF("Creating linked list ...");
 	list->head = NULL;
 	list->last_node = NULL;
 	list->print_val = ptr_print_val;
-	printf(" Created.\n"); 
+	DEBUG_PRINTF(" Created.\n"); 
 }
 
 void ll_set_data_validation_callback(int (*ptr_callback_validate)(void *data))
@@ -30,7 +38,7 @@ void ll_set_data_validation_callback(int (*ptr_callback_validate)(void *data))
 
 void ll_add_end(ll_t *list, void *value)
 {	
-	printf("add function has been called!!!	");
+	DEBUG_PRINTF("ll_add_end function has been called!!!	");
 	
 	if (callback_validate != NULL) {
 		if (callback_validate(value) != 0)
@@ -39,7 +47,7 @@ void ll_add_end(ll_t *list, void *value)
 	
 	node_t *new_node = malloc(sizeof(node_t));	
 	if (new_node == NULL) {
-		printf("Memory allocation for new_node failed! (add) \n");
+		DEBUG_PRINTF("Memory allocation for new_node failed! (add) \n");
 		return;
 	}
 	new_node->next = NULL;
@@ -52,16 +60,16 @@ void ll_add_end(ll_t *list, void *value)
 		list->last_node->next = new_node;    //link new_node with last node if linked list is not empty
 	}
 	
-	printf("Node with value: ");
+	DEBUG_PRINTF("Node with value: ");
 	list->print_val(new_node->val);
-	printf(" added!\n");
+	DEBUG_PRINTF(" added!\n");
 	list->last_node = new_node;
 }
 
 void ll_delete(ll_t *list, void *value, int (*comparator)(void *first, void* second))
 {
-	printf("delete function has been called!!!	");
-	if (validate_head(list, "delete") == -1) {    //validate head list		
+	DEBUG_PRINTF("ll_delete function has been called!!!	");
+	if (validate_head(list, "ll_delete") == -1) {    //validate head list		
 		return;    
 	}
 	node_t *temp = list->head;
@@ -70,9 +78,9 @@ void ll_delete(ll_t *list, void *value, int (*comparator)(void *first, void* sec
 
 	if (comparator(list->head->val, value) == 0) {
 		list->head = temp->next;
-		printf("Node head with value: ");
+		DEBUG_PRINTF("Node head with value: ");
 		list->print_val(temp->val); 
-		printf("deleted!\n");
+		DEBUG_PRINTF("deleted!\n");
 		free(temp);
 		list->last_node = NULL;    //correctly reset last_node in this corner case
 		return;
@@ -81,10 +89,10 @@ void ll_delete(ll_t *list, void *value, int (*comparator)(void *first, void* sec
 	while (temp != NULL) {
 		count_element++;
 		if (comparator(temp->val, value) == 0) {
-			printf("Node no.%d ",count_element);
-			printf("with value: ");
+			DEBUG_PRINTF("Node no.%d ",count_element);
+			DEBUG_PRINTF("with value: ");
 			list->print_val(temp->val); 
-			printf("deleted!\n");
+			DEBUG_PRINTF("deleted!\n");
 			if (temp->next != NULL)
 				previous_node->next = temp->next;
 			else {
@@ -102,9 +110,28 @@ void ll_delete(ll_t *list, void *value, int (*comparator)(void *first, void* sec
 		temp = temp->next;
 	}
 	
-	printf("Node head with value: ");
+	DEBUG_PRINTF("Node head with value: ");
 	list->print_val(value); 
-	printf("doesn't exist!\n");
+	DEBUG_PRINTF("doesn't exist!\n");
+}
+
+node_t *ll_search_node(ll_t *list, void *value, int (*comparator)(void *first, void* second))
+{
+	DEBUG_PRINTF("ll_search_node function has been called!!!	");
+	if (validate_head(list, "ll_search_node") == -1) {    //validate head list		
+		return NULL;
+	}
+	
+	node_t *temp = list->head;
+	while (temp != NULL) {
+		if (comparator(temp->val, value) == 0) {
+			DEBUG_PRINTF("Node found!\n");
+			return temp;		
+		}
+		temp = temp->next;
+	}
+	DEBUG_PRINTF("Node not found! Returning NULL.\n");
+	return NULL;
 }
 
 void swap(node_t *a, node_t *b)    //used for sort_list
@@ -116,11 +143,11 @@ void swap(node_t *a, node_t *b)    //used for sort_list
 
 void ll_sort_list(ll_t *list, int (*comparator)(void *first, void* second))    //bubble sort
 {
-	printf("sort_list has been called!!!    ");
+	DEBUG_PRINTF("sort_list has been called!!!    ");
 	int swapped;
 	node_t *ptr2 = NULL;
 	node_t *ptr1 = NULL;    //was uninitialized
-	if (validate_head(list, "sort_list") == -1) {    //validate head list
+	if (validate_head(list, "ll_sort_list") == -1) {    //validate head list
 			return;
 	}
 	
@@ -138,12 +165,12 @@ void ll_sort_list(ll_t *list, int (*comparator)(void *first, void* second))    /
 		ptr2 = ptr1;
 	}
 	while (swapped);
-	printf("Linked list has been sorted! \n");
+	DEBUG_PRINTF("Linked list has been sorted! \n");
 }
 
 void ll_flush_list(ll_t *list)
 {
-	if (validate_head(list, "flush_list") == -1) {    //validate head list
+	if (validate_head(list, "ll_flush_list") == -1) {    //validate head list
 		return;
 	}
 	
@@ -153,14 +180,14 @@ void ll_flush_list(ll_t *list)
 		list->head = list->head->next;
 		free(temp);	
 	}
-	printf("Linked list has been deleted!\n");
+	DEBUG_PRINTF("Linked list has been deleted!\n");
 }
 
 void ll_print_list(ll_t *list)
 {	
-	printf("print_list function has been called!!!	");
-	printf("Printing linked list: ");
-	if (validate_head(list, "print_list") == -1) {    //validate head list
+	DEBUG_PRINTF("print_list function has been called!!!	");
+	DEBUG_PRINTF("Printing linked list: ");
+	if (validate_head(list, "ll_print_list") == -1) {    //validate head list
 		return;
 	}
 	
@@ -169,8 +196,8 @@ void ll_print_list(ll_t *list)
 		list->print_val(temp->val);    //use function pointer	
 		temp = temp->next;
 		if (temp != NULL) {
-			printf(" ---> ");
+			DEBUG_PRINTF(" ---> ");
 		}
 	}
-	printf("\n");
+	DEBUG_PRINTF("\n");
 }
