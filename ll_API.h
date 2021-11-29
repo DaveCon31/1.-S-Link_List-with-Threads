bb6787eefc7dc36f85afaaaa948d35c9b6127fae
DaveCon31 <davidviorel54@yahoo.com>
2021-11-29 14:32:38 +0200

    remade delete function, remade comparator as list member -> only ll_create will require comparator function

--- ll_API.h
+++ ll_API.h
@@ -14,19 +14,19 @@ void ll_set_data_validation_callback(int (*ptr_callback_validate)(void *data));
 
 //initialize linked list with last_node & head = NULL
 //MUST BE CALLED FOR EVERY NEW LIST CREATED OTHERWISE SEGFAULT
-ll_t *ll_create(void (*print_val)(void *val));
+ll_t *ll_create(void (*print_val)(void *val), int (*comparator)(void *first, void* second));
 
 //adds a node at the end of the list with a value
 void ll_add_end(ll_t *list, void *value);
 
 //removes the node with a value
-void ll_delete(ll_t *list, void *value, int (*comparator)(void *first, void* second));
+void ll_delete(ll_t *list, void *value);
 
 //returns the address of node with specified data
-node_t *ll_search_node(ll_t *list, void *value, int (*comparator)(void *first, void* second));
+node_t *ll_search_node(ll_t *list, void *value);
 
 //sorting the list using bubble sort algorithm
-void ll_sort_list(ll_t *list, int (*comparator)(void *first, void* second));
+void ll_sort_list(ll_t *list);
 
 //deletes entire list, deallocated everything
 void ll_flush_list(ll_t *list);
@@ -35,6 +35,6 @@ void ll_flush_list(ll_t *list);
 void ll_print_list(ll_t *list);
 
 //destroy list
-void ll_destroy_list(ll_t *list);
+void ll_destroy_list(ll_t **list);
 
 #endif		//ll_API
